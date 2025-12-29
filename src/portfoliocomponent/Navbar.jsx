fae7013c778335dda6a9b3a5d25e8fdb27ef55bd
Jeet Nathwani <162664998+JeetNathwani26@users.noreply.github.com>
2025-12-29 08:20:31 +0530

    Fix formatting in Navbar component

--- src/portfoliocomponent/Navbar.jsx
+++ src/portfoliocomponent/Navbar.jsx
@@ -137,7 +137,7 @@ const Navbar = () => {
       )}
 
                       {showLogin && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50  "}>
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"}>
     
           <Loginpages />
         </div>
